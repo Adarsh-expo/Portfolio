288b0d3ce2ee6d268ef30bd95c2ea8974cfae2dd
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-06-09 17:39:23 +0530

    hh

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -7,7 +7,7 @@ function Skill({section2}) {
     <div name='about'    ref={section2} className='skill'>
 
 <div className='about'>Things I Do</div>
-<p className='para'>I am a well skilled and well crafted we application developer<br/>with well understanding
+<p className='para'>I am a well skilled and well crafted web application developer<br/>with well understanding
     of frontend and backend development<br/>I do create visual appealing and responsive web application</p>
 <div className='skillvalue'><img className='img'   src={ui}/>UI Design</div>
 <div className='skillvalue'><img className='img'   src={web}/>Web Application Design</div>
