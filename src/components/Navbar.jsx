a78f6327fcd8ed07c1ef019458b52be07c65edb6
adarsh kumar <adarshupadhyay231501@gmail.com>
2024-06-09 16:14:12 +0530

    added responsiveness in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useRef } from 'react';
 import '../styles/navbar.css';
 import contact from '/contact1.png';
 import { Link, Button,scroller, Element, Events, animateScroll as scroll, scrollSpy } from 'react-scroll';
 import port from '/port.png'
+import 'remixicon/fonts/remixicon.css'
 
 function Navbar() {
 
-
+const[screen,setscreen]=useState(window.innerWidth<800);
+const[showmenu,setmenu]=useState(false);
  
+window.addEventListener('resize',()=>{
+setscreen(window.innerWidth<766?true:false)
 
+})
 
   const options = {
     // Your options here, for example:
@@ -19,19 +24,37 @@ function Navbar() {
 function reachhome(pxo) {
   scroll.scrollTo(pxo, options);
 }
-  return (
-    <div className='navbar'>
+  return (<>
+    
+    <div  style={{
+        padding:screen&&'0 0.01rem',
+      }}   className='navbar'>
       <img src={port} alt='Logo' className='portlogo'/>
-      <div className='laptopmenu'>
-        <Link  onClick={()=>{reachhome(50)}}   className='Listitem'>Home</Link>
-        <Link  onClick={()=>{reachhome(570)}}  className='Listitem'>About</Link>
-        <Link  onClick={()=>{reachhome(1010)}}   to='portfolio'  className='Listitem'>Portfolio</Link>
-        <Link to='/clients' className='Listitem'>Clients</Link>
-      </div>
-      <button className='laptopmenubtn'>
+{
+screen ?<div><button onClick={()=>{setmenu((pre)=>!pre)}} className='burger'><i class="ri-menu-fill "></i></button></div>:
+<div className='laptopmenu'>
+
+<Link  onClick={()=>{reachhome(50)}}   className='Listitem'>Home</Link>
+<Link  onClick={()=>{reachhome(570)}}  className='Listitem'>About</Link>
+<Link  onClick={()=>{reachhome(1010)}}   to='portfolio'  className='Listitem'>Portfolio</Link>
+<Link to='/clients' className='Listitem'>Clients</Link>
+</div>
+
+
+}
+     
+      <button   className='laptopmenubtn'>
         <img  src={contact} alt='Contact Icon' className='laptopmenuimg'/>Contact Me
       </button>
-    </div>
+
+      {
+        showmenu&&<div className='menu'><Link  onClick={()=>{reachhome(50)}}   className='Listitem'>Home</Link>
+<Link  onClick={()=>{reachhome(570)}}  className='Listitem'>About</Link>
+<Link  onClick={()=>{reachhome(1010)}}   to='portfolio'  className='Listitem'>Portfolio</Link>
+<Link to='/clients' className='Listitem'>Clients</Link></div>
+      }
+      
+    </div></>
   );
 }
 
